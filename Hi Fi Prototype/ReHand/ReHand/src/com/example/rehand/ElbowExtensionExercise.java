a6c582261ae45e467e47973b6058431aee48a4f7
zi-yang-zhang <zi.y.zhang@mail.mcgill.ca>
2014-03-07 01:10:19 -0500

    accelerometer example with elbow extension exercise

--- Hi Fi Prototype/ReHand/ReHand/src/com/example/rehand/ElbowExtensionExercise.java
+++ Hi Fi Prototype/ReHand/ReHand/src/com/example/rehand/ElbowExtensionExercise.java
@@ -1,17 +1,34 @@
 package com.example.rehand;
 
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
-public class ElbowExtensionExercise extends Activity {
-
+public class ElbowExtensionExercise extends Activity implements SensorEventListener {
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private final float NOISE = (float) 0.3;
+	private boolean mInitialized;
+	private float mLastX, mLastY, mLastZ;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_elbow_extension_exercise);
+
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 	}
 
 	@Override
@@ -20,9 +37,48 @@ public class ElbowExtensionExercise extends Activity {
 		getMenuInflater().inflate(R.menu.elbow_extension_exercise, menu);
 		return true;
 	}
+	@Override
+	public void onSensorChanged(SensorEvent event){
+		final TextView xPosition = (TextView) findViewById(R.id.xPosition);
+		final TextView yPosition = (TextView) findViewById(R.id.yPosition);
+		final TextView zPosition = (TextView) findViewById(R.id.zPosition);
+		float x = event.values[0];
+		float y = event.values[1];
+		float z = event.values[2];
+		if (!mInitialized) {
+			mLastX = x;
+			mLastY = y;
+			mLastZ = z;
+			xPosition.setText("X: " +"0.0");
+			yPosition.setText("Y: " +"0.0");
+			zPosition.setText("Z: " +"0.0");
+			mInitialized = true;
+		}else {
+			float deltaX = Math.abs(mLastX - x);
+			float deltaY = Math.abs(mLastY - y);
+			float deltaZ = Math.abs(mLastZ - z);
+			if (deltaX < NOISE) deltaX = (float)0.0;
+			if (deltaY < NOISE) deltaY = (float)0.0;
+			if (deltaZ < NOISE) deltaZ = (float)0.0;
+			mLastX = x;
+			mLastY = y;
+			mLastZ = z;
+			xPosition.setText("X: " +Float.toString(deltaX));
+			yPosition.setText("Y: " +Float.toString(deltaY));
+			zPosition.setText("Z: " +Float.toString(deltaZ));
+			
+		}
+		
+	}
 	public void goToElbowExtensionResult(View view){
 		Intent intent = new Intent(this, ElbowExtensionResult.class);
 		startActivity(intent);
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
