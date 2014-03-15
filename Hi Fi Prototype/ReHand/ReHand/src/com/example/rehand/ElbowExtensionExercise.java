87e3ed0f7230c03a44d940fced0bfc1d1b7f29a0
zi-yang-zhang <zi.y.zhang@mail.mcgill.ca>
2014-03-15 02:55:35 -0400

    Timer implemented wit count down

--- Hi Fi Prototype/ReHand/ReHand/src/com/example/rehand/ElbowExtensionExercise.java
+++ Hi Fi Prototype/ReHand/ReHand/src/com/example/rehand/ElbowExtensionExercise.java
@@ -29,6 +29,8 @@ public class ElbowExtensionExercise extends Activity implements SensorEventListe
 	private float mLastX, mLastY, mLastZ;
 	float movementZ;
 	private boolean mInitialized;
+	private boolean gameStart;
+	private int timer;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -37,7 +39,7 @@ public class ElbowExtensionExercise extends Activity implements SensorEventListe
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
 		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-	*/
+	*/	
 	    mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 	    mOrientation = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
 	    mSensorManager.registerListener(this, mOrientation, SensorManager.SENSOR_DELAY_NORMAL);
@@ -54,38 +56,46 @@ public class ElbowExtensionExercise extends Activity implements SensorEventListe
 		final TextView xPosition = (TextView) findViewById(R.id.xPosition);
 		final TextView yPosition = (TextView) findViewById(R.id.yPosition);
 		final TextView zPosition = (TextView) findViewById(R.id.zPosition);
+		final TextView countDown = (TextView) findViewById(R.id.countDown);
 		final TextView horizontalMovement = (TextView) findViewById(R.id.horizontalMovement);
 	    float z = event.values[0];
 	    float x = event.values[1];
 	    float y = event.values[2];
 
-	    if (!mInitialized) {
+	    timer = timer +1;
+	    if(timer/5 == 1){
+	    	countDown.setText("3");
+	    }else if(timer/5 == 2){
+	    	countDown.setText("2");
+	    }else if(timer/5 == 3){
+	    	countDown.setText("1");
+	    }else if(timer/5 == 4){
+	    	countDown.setText("");
 	    	mLastX = x;
 			mLastY = y;
 			mLastZ = z;
-			xPosition.setText("X: " +"0.0");
-			yPosition.setText("Y: " +"0.0");
-			zPosition.setText("Z: " +"0.0");
+			xPosition.setText("");
+			yPosition.setText("");
+			zPosition.setText("");
+			horizontalMovement.setText("");
 	    	movementZ=0;
-	    	mInitialized=true;
-	    }else{
-				float deltaX = Math.abs(mLastX - x);
-				float deltaY = Math.abs(mLastY - y);
-				float deltaZ = Math.abs(mLastZ - z);
-				if (deltaX < NOISE) deltaX = mLastX;else deltaX = x;
-				if (deltaY < NOISE) deltaY = mLastY;else deltaY = y;
-				if (deltaZ < NOISE) deltaZ = mLastZ;else deltaZ = z;movementZ = movementZ + (deltaZ-mLastZ);
-				
-				mLastX = deltaX;
-				mLastY = deltaY;
-				mLastZ = deltaZ;
-				
-				horizontalMovement.setText("moved: " +Float.toString(movementZ));
-				xPosition.setText("X: " +Float.toString(deltaX));
-				yPosition.setText("Y: " +Float.toString(deltaY));
-				zPosition.setText("Z: " +Float.toString(deltaZ));
+	    }else if(timer/5>4){
+			float deltaX = Math.abs(mLastX - x);
+			float deltaY = Math.abs(mLastY - y);
+			float deltaZ = Math.abs(mLastZ - z);
+			if (deltaX < NOISE) deltaX = mLastX;else deltaX = x;
+			if (deltaY < NOISE) deltaY = mLastY;else deltaY = y;
+			if (deltaZ < NOISE) deltaZ = mLastZ;else deltaZ = z;movementZ = movementZ + (deltaZ-mLastZ);
+			
+			mLastX = deltaX;
+			mLastY = deltaY;
+			mLastZ = deltaZ;
+			
+			horizontalMovement.setText("moved: " +Float.toString(movementZ));
+			xPosition.setText("X: " +Float.toString(deltaX));
+			yPosition.setText("Y: " +Float.toString(deltaY));
+			zPosition.setText("Z: " +Float.toString(deltaZ));
 	    }
-	    System.out.println(movementZ);
 
 		/*
 		final TextView xPosition = (TextView) findViewById(R.id.xPosition);
